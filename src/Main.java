735e1136548ff6afcdcc01f6a70e4e95bc71e4b5
Ana Laura Marques <lauramarques181@gmail.com>
2024-08-30 20:52:20 -0300

    fix: rename 'hotelTest01' to 'Main'

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ package src;
 import java.util.List;
 import java.util.Scanner;
 
-public class HotelTest01 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean continuar = true;
